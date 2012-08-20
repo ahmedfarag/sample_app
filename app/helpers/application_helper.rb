990b749d78298ea014f009adf37d7e45d463f656
ahmedfarag <ahmed.farag1111@gmail.com>
2012-08-20 09:42:20 +0200

    Add Title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  #Return default Title if no one was defined
+  def title 
+    base_title = "Sample App"
+    if @title.nil?
+      base_title
+    else
+     "#{base_title} | #{@title}"
+    end
+  end 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe PagesController do
+  
+  before(:each) do
+    @base_title = "Sample App"
+  end
   render_views
   describe "GET 'home'" do
     it "returns http success" do
@@ -10,7 +14,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Sample App | Home")
+      response.should have_selector("title", :content => "#{@base_title} | Home")
     end
     
     it "should have a non-empty body" do
@@ -27,7 +31,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Sample App | Contact")
+      response.should have_selector("title", :content => "#{@base_title} | Contact")
     end
   end
 
@@ -39,7 +43,7 @@ describe PagesController do
     
       it "should have the right title" do
         get 'about'
-        response.should have_selector("title", :content => "Sample App | About")
+        response.should have_selector("title", :content => "#{@base_title} | About")
       end    
     end   
 end
